c34e4217606b1e1da423ba1c09373407fb05a413
insanitywholesale <popomastor@gmail.com>
2021-05-01 15:57:18 +0300

    change how terraform is used

---
+++ terraform/proxmox/pve-0/.terraform.lock.hcl
@@ -0,0 +1,22 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/telmate/proxmox" {
+  version     = "2.6.9"
+  constraints = "2.6.9"
+  hashes = [
+    "h1:F5jSFLTa2XApeZeRNZiyr+7F2nvLlp/QbSZ5DnwmVEw=",
+    "zh:2efc30f64559427b5ba3b23f24d54dffc42f8a6a701f86a51a6acadee80cd8fe",
+    "zh:40774b805320367499d0e6d4a48528835803e21821200a2d1e7ae4a8ab6ed468",
+    "zh:578f61bbbeb5d1cfef5b0fc952d525d2cee882ed5fc9be5ee7670df7dbf0e501",
+    "zh:6a2078a0a6512a905049dc9112bd2fe4786879eae32dab2019117b4fe2232f7c",
+    "zh:891f59224c6166e59bc250d6153652ff43bd61be27fcb2ec50e181c6ef3201d1",
+    "zh:962fb5fed1b3cfb0e15adfa807184e5ab0aa3ce2a7e1b095622dc753cf322eda",
+    "zh:9848d76f58c35545f072888481bc5989862337ec40870479aae926a55a6772ec",
+    "zh:acf4e2c8a238fb8295d42c554f7b75b7ab0acabcb4586a9cb1388376e8fb1a7c",
+    "zh:b250db375b4c00d9eeb1ea51396070567b4aed1028751358b9f48657e68b915a",
+    "zh:bba2b22a12495e7aacb212cbbefee38020b1b454706e722b59976e0790c00c65",
+    "zh:d160471b3dc36d3ab3377fd380e50c5d6fed866c264ef1a578b5c19001b20822",
+    "zh:f0aa60ea8b11578ef7e60650a0a286f529a3246b2451f2d46ce5b8b1722319ea",
+  ]
+}
---
+++ terraform/proxmox/pve1/main.tf
@@ -0,0 +1,64 @@
+terraform {
+	required_providers {
+		proxmox = {
+			source = "Telmate/proxmox"
+			version = "2.6.9"
+		}
+	}
+}
+
+provider "proxmox" {
+	pm_tls_insecure = true
+	pm_api_url = "https://pve1.hell:8006/api2/json"
+	pm_password = "failfail"
+	pm_user = "root@pam"
+}
+
+resource "proxmox_vm_qemu" "proxmox_vm_k3s" {
+
+	count = 3
+	name = "deb-k3s-${count.index + 1}"
+	target_node = "pve1"
+
+	clone = "debian-templ"
+	os_type = "cloud-init"
+	cores = 2
+	sockets = "1"
+	cpu = "host"
+	memory = 2560
+	scsihw = "virtio-scsi-pci"
+	bootdisk = "virtio0"
+	agent = 1
+	guest_agent_ready_timeout = 120
+
+	disk {
+		size = "33G"
+		type = "virtio"
+		storage = "local-zfs"
+		iothread = 1
+	}
+
+	network {
+		model = "virtio"
+		bridge = "vmbr0"
+	}
+
+/*
+	lifecycle {
+		ignore_changes = [
+			cipassword,
+			network,
+			desc,
+		]
+	}
+*/
+
+	ipconfig0 = "ip=192.168.4.5${count.index + 4}/16,gw=192.168.0.1"
+
+	sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5jzKi37jm3517bqThbw+7LR/GXm3qC6Az5F+ZUa36vYM7Ygk2K5bWcFIL2YUCrkL5jfSsvoowONjCAxyuoyxtW4MJxnQLyq4u4yDsRC7YvBPAKZUYaHwnbkCfDs5a75dEFOoDxCA0DY2GrhqzBndaTcCfl0fZ4vN+9LcKOb1dSKiHeHvsh35YNtwntbL21meo+hiycUEgGwNe9/4kxKpdGTr7HvbeX2Fjm/UZBZIJKVcGop/3gCHXYnKH+OY5zc8cmt9Jg4CIwEqrSKeOX0bE8LSPRpVRXH4v8OcMaMei/HQejlH8NBwybEdJ4mhl8vHaFEjDbIWoOujmiRQF2263 angle@puddle"
+
+}
+
+output "rancher_ips" {
+	value = proxmox_vm_qemu.proxmox_vm_k3s.*.ipconfig0
+}
