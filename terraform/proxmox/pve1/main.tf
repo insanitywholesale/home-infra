a266921c831268526ab59eb57a251caa88008e5d
lhz <pepitsizous@zoho.com>
2021-07-09 21:08:57 +0300

    modify pve1 k3s VM terraform config

--- terraform/proxmox/pve1/main.tf
+++ terraform/proxmox/pve1/main.tf
@@ -2,7 +2,7 @@ terraform {
 	required_providers {
 		proxmox = {
 			source = "Telmate/proxmox"
-			version = "2.6.9"
+			version = "2.7.1"
 		}
 	}
 }
@@ -17,7 +17,7 @@ provider "proxmox" {
 resource "proxmox_vm_qemu" "proxmox_vm_k3s" {
 
 	count = 3
-	name = "deb-k3s-${count.index + 1}"
+	name = "deb-k3s2-${count.index + 1}"
 	target_node = "pve1"
 
 	clone = "debian-templ"
@@ -25,11 +25,12 @@ resource "proxmox_vm_qemu" "proxmox_vm_k3s" {
 	cores = 2
 	sockets = "1"
 	cpu = "host"
-	memory = 2560
+	memory = 3084
 	scsihw = "virtio-scsi-pci"
 	bootdisk = "virtio0"
 	agent = 1
 	guest_agent_ready_timeout = 120
+	onboot = false
 
 	disk {
 		size = "33G"
@@ -43,17 +44,7 @@ resource "proxmox_vm_qemu" "proxmox_vm_k3s" {
 		bridge = "vmbr0"
 	}
 
-/*
-	lifecycle {
-		ignore_changes = [
-			cipassword,
-			network,
-			desc,
-		]
-	}
-*/
-
-	ipconfig0 = "ip=192.168.4.5${count.index + 4}/16,gw=192.168.0.1"
+	ipconfig0 = "ip=192.168.5.5${count.index + 4}/16,gw=192.168.0.1"
 
 	sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5jzKi37jm3517bqThbw+7LR/GXm3qC6Az5F+ZUa36vYM7Ygk2K5bWcFIL2YUCrkL5jfSsvoowONjCAxyuoyxtW4MJxnQLyq4u4yDsRC7YvBPAKZUYaHwnbkCfDs5a75dEFOoDxCA0DY2GrhqzBndaTcCfl0fZ4vN+9LcKOb1dSKiHeHvsh35YNtwntbL21meo+hiycUEgGwNe9/4kxKpdGTr7HvbeX2Fjm/UZBZIJKVcGop/3gCHXYnKH+OY5zc8cmt9Jg4CIwEqrSKeOX0bE8LSPRpVRXH4v8OcMaMei/HQejlH8NBwybEdJ4mhl8vHaFEjDbIWoOujmiRQF2263 angle@puddle"
 
