97e309941dc3dcecab6dcfe66acbb9175d8ff925
insanitywholesale <popomastor@gmail.com>
2022-02-17 22:22:34 +0200

    oopsie, forgot to update main.tf

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -193,7 +193,7 @@ resource "proxmox_vm_qemu" "proxmox_vm_k3s2" {
 
 resource "local_file" "hosts_cfg_k3s" {
 	content = templatefile(
-		"${path.module}/inventory-k3s.tftpl",
+		"${path.module}/inventory.tftpl",
 		{
 			k3s_hosts = proxmox_vm_qemu.proxmox_vm_k3s.*,
 			k3s2_hosts = proxmox_vm_qemu.proxmox_vm_k3s2.*,
