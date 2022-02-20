167ed8b2c1c32155d7b2195eb6f9d6bd6ced7e83
insanitywholesale <popomastor@gmail.com>
2022-02-20 13:57:27 +0200

    reformat main.tf

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -65,6 +65,48 @@ resource "proxmox_vm_qemu" "proxmox_vm_k3s" {
 	}
 }
 
+resource "proxmox_vm_qemu" "proxmox_vm_k3s2" {
+	provider = proxmox.pve1
+	count = 5
+	name = "deb-k3s2-${count.index + 1}"
+	target_node = "pve1"
+
+	/* change to debian-templ for 11 */
+	clone = "debian-tmpl"
+	os_type = "cloud-init"
+	cores = 2
+	sockets = "1"
+	cpu = "host"
+	memory = 3084
+	scsihw = "virtio-scsi-pci"
+	bootdisk = "virtio0"
+	agent = 0
+	onboot = true
+
+	disk {
+		size = "30G"
+		type = "virtio"
+		storage = "local-zfs"
+	}
+
+	network {
+		model = "virtio"
+		bridge = "vmbr0"
+	}
+
+	ipconfig0 = "ip=192.168.15.5${count.index + 1}/16,gw=192.168.0.1"
+
+	sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5jzKi37jm3517bqThbw+7LR/GXm3qC6Az5F+ZUa36vYM7Ygk2K5bWcFIL2YUCrkL5jfSsvoowONjCAxyuoyxtW4MJxnQLyq4u4yDsRC7YvBPAKZUYaHwnbkCfDs5a75dEFOoDxCA0DY2GrhqzBndaTcCfl0fZ4vN+9LcKOb1dSKiHeHvsh35YNtwntbL21meo+hiycUEgGwNe9/4kxKpdGTr7HvbeX2Fjm/UZBZIJKVcGop/3gCHXYnKH+OY5zc8cmt9Jg4CIwEqrSKeOX0bE8LSPRpVRXH4v8OcMaMei/HQejlH8NBwybEdJ4mhl8vHaFEjDbIWoOujmiRQF2263 angle@puddle"
+
+	lifecycle {
+		ignore_changes = [
+			cipassword,
+			network,
+			desc,
+		]
+	}
+}
+
 resource "proxmox_vm_qemu" "reverse-proxy-ngx" {
 	provider = proxmox.pve0
 	count = 1
@@ -149,47 +191,6 @@ resource "proxmox_vm_qemu" "reverse-proxy-hap" {
 	}
 }
 
-resource "proxmox_vm_qemu" "proxmox_vm_k3s2" {
-	provider = proxmox.pve1
-	count = 5
-	name = "deb-k3s2-${count.index + 1}"
-	target_node = "pve1"
-
-	/* change to debian-templ for 11 */
-	clone = "debian-tmpl"
-	os_type = "cloud-init"
-	cores = 2
-	sockets = "1"
-	cpu = "host"
-	memory = 3084
-	scsihw = "virtio-scsi-pci"
-	bootdisk = "virtio0"
-	agent = 0
-	onboot = true
-
-	disk {
-		size = "30G"
-		type = "virtio"
-		storage = "local-zfs"
-	}
-
-	network {
-		model = "virtio"
-		bridge = "vmbr0"
-	}
-
-	ipconfig0 = "ip=192.168.15.5${count.index + 1}/16,gw=192.168.0.1"
-
-	sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5jzKi37jm3517bqThbw+7LR/GXm3qC6Az5F+ZUa36vYM7Ygk2K5bWcFIL2YUCrkL5jfSsvoowONjCAxyuoyxtW4MJxnQLyq4u4yDsRC7YvBPAKZUYaHwnbkCfDs5a75dEFOoDxCA0DY2GrhqzBndaTcCfl0fZ4vN+9LcKOb1dSKiHeHvsh35YNtwntbL21meo+hiycUEgGwNe9/4kxKpdGTr7HvbeX2Fjm/UZBZIJKVcGop/3gCHXYnKH+OY5zc8cmt9Jg4CIwEqrSKeOX0bE8LSPRpVRXH4v8OcMaMei/HQejlH8NBwybEdJ4mhl8vHaFEjDbIWoOujmiRQF2263 angle@puddle"
-
-	lifecycle {
-		ignore_changes = [
-			cipassword,
-			network,
-			desc,
-		]
-	}
-}
 
 /* this is also interesting https://github.com/NatiSayada/k3s-proxmox-terraform-ansible/blob/main/terraform/main.tf */
 resource "local_file" "hosts_cfg_k3s" {
