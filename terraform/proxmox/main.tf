6e62e0dfb8405f822429159a3e3242060f4eeab9
insanitywholesale <popomastor@gmail.com>
2022-07-17 11:16:18 +0300

    add arch in template, script, ansible, terraform

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -40,7 +40,7 @@ resource "proxmox_vm_qemu" "proxmox_vm_k3s" {
 	memory = 2560
 	scsihw = "virtio-scsi-pci"
 	bootdisk = "virtio0"
-	agent = 0
+	agent = 1
 	onboot = true
 
 	disk {
@@ -82,7 +82,7 @@ resource "proxmox_vm_qemu" "proxmox_vm_k3s2" {
 	memory = 3084
 	scsihw = "virtio-scsi-pci"
 	bootdisk = "virtio0"
-	agent = 0
+	agent = 1
 	onboot = true
 
 	disk {
@@ -97,6 +97,48 @@ resource "proxmox_vm_qemu" "proxmox_vm_k3s2" {
 	}
 
 	ipconfig0 = "ip=192.168.15.5${count.index + 1}/16,gw=192.168.0.1"
+
+	sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5jzKi37jm3517bqThbw+7LR/GXm3qC6Az5F+ZUa36vYM7Ygk2K5bWcFIL2YUCrkL5jfSsvoowONjCAxyuoyxtW4MJxnQLyq4u4yDsRC7YvBPAKZUYaHwnbkCfDs5a75dEFOoDxCA0DY2GrhqzBndaTcCfl0fZ4vN+9LcKOb1dSKiHeHvsh35YNtwntbL21meo+hiycUEgGwNe9/4kxKpdGTr7HvbeX2Fjm/UZBZIJKVcGop/3gCHXYnKH+OY5zc8cmt9Jg4CIwEqrSKeOX0bE8LSPRpVRXH4v8OcMaMei/HQejlH8NBwybEdJ4mhl8vHaFEjDbIWoOujmiRQF2263 angle@puddle"
+
+	lifecycle {
+		ignore_changes = [
+			cipassword,
+			network,
+			desc,
+		]
+	}
+}
+
+resource "proxmox_vm_qemu" "proxmox_vm_arch" {
+	provider = proxmox.pve1
+	count = 1
+	name = "arch-${count.index + 1}"
+	target_node = "pve1"
+
+	/* change to debian-templ for 11 */
+	clone = "arch-tmpl"
+	os_type = "cloud-init"
+	cores = 2
+	sockets = "1"
+	cpu = "host"
+	memory = 1024
+	scsihw = "virtio-scsi-pci"
+	bootdisk = "virtio0"
+	agent = 1
+	onboot = true
+
+	disk {
+		size = "20G"
+		type = "virtio"
+		storage = "local-zfs"
+	}
+
+	network {
+		model = "virtio"
+		bridge = "vmbr0"
+	}
+
+	ipconfig0 = "ip=192.168.3.${count.index}/16,gw=192.168.0.1"
 
 	sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5jzKi37jm3517bqThbw+7LR/GXm3qC6Az5F+ZUa36vYM7Ygk2K5bWcFIL2YUCrkL5jfSsvoowONjCAxyuoyxtW4MJxnQLyq4u4yDsRC7YvBPAKZUYaHwnbkCfDs5a75dEFOoDxCA0DY2GrhqzBndaTcCfl0fZ4vN+9LcKOb1dSKiHeHvsh35YNtwntbL21meo+hiycUEgGwNe9/4kxKpdGTr7HvbeX2Fjm/UZBZIJKVcGop/3gCHXYnKH+OY5zc8cmt9Jg4CIwEqrSKeOX0bE8LSPRpVRXH4v8OcMaMei/HQejlH8NBwybEdJ4mhl8vHaFEjDbIWoOujmiRQF2263 angle@puddle"
 
