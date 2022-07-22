5a69b7a795cdd6eb18c8d83c9a85a981e35e2218
insanitywholesale <popomastor@gmail.com>
2022-07-22 14:52:47 +0300

    add convenience scripts

---
+++ terraform/proxmox/scripts/tear_down.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ansible-playbook -i inventory/k3s-inventory.ini playbooks/k3s-reset.yml
+ansible-playbook -i inventory/k3s-inventory.ini playbooks/k3s-ha-reset.yml
+ansible-playbook -i inventory/k3s-inventory.ini playbooks/k3s-ha-reset.yml
+ansible-playbook -i inventory/k3s-inventory.ini playbooks/kates.yml
