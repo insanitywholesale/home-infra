01e3b8cab171f082e03b972c953ffeea170d7bb2
insanitywholesale <popomastor@gmail.com>
2022-07-22 20:45:32 +0300

    enable qemu-guest-agent

--- terraform/proxmox/playbooks/files/setup-debian-10-cloudinit.sh
+++ terraform/proxmox/playbooks/files/setup-debian-10-cloudinit.sh
@@ -2,14 +2,14 @@
 
 if [ -z "$1" ]
 then
-	VMID=9001
+	VMID=9010
 else
 	VMID="$1"
 fi
 
 if [ -z "$2" ]
 then
-	VMNAME="debian-tmpl"
+	VMNAME="debian-10-template"
 else
 	VMNAME="$2"
 fi
@@ -21,12 +21,12 @@ then
 	echo "VM with ${VMID} already exists; exiting gracefully"
 	exit 0
 else
-	qm create "${VMID}" -name "${VMNAME}" -memory 1024 -net0 virtio,bridge=vmbr0 -cores 1 -sockets 1 -cpu cputype=kvm64 -description "debbie image" -kvm 1 -numa 1
-	qm importdisk "${VMID}" debian-10-openstack-amd64.qcow2 local-zfs
+	qm create "${VMID}" -name "${VMNAME}" -memory 1024 -net0 virtio,bridge=vmbr0 -cores 1 -sockets 1 -cpu cputype=kvm64 -description "debian 10 template" -kvm 1 -numa 1
+	qm importdisk "${VMID}" debian-10-amd64.qcow2 local-zfs
 	qm set "${VMID}" -scsihw virtio-scsi-pci -virtio0 local-zfs:vm-"${VMID}"-disk-0
 	qm set "${VMID}" -serial0 socket
 	qm set "${VMID}" -boot c -bootdisk virtio0
-#	qm set "${VMID}" -agent 1
+	qm set "${VMID}" -agent 1
 	qm set "${VMID}" -hotplug disk,network,usb,memory,cpu
 	qm set "${VMID}" -vcpus 1
 	qm set "${VMID}" -vga qxl
--- terraform/proxmox/playbooks/files/setup-debian-11-cloudinit.sh
+++ terraform/proxmox/playbooks/files/setup-debian-11-cloudinit.sh
@@ -2,14 +2,14 @@
 
 if [ -z "$1" ]
 then
-	VMID=9002
+	VMID=9011
 else
 	VMID="$1"
 fi
 
 if [ -z "$2" ]
 then
-	VMNAME="debian-templ"
+	VMNAME="debian-11-template"
 else
 	VMNAME="$2"
 fi
@@ -21,12 +21,12 @@ then
 	echo "VM with ${VMID} already exists; exiting gracefully"
 	exit 0
 else
-	qm create "${VMID}" -name "${VMNAME}" -memory 1024 -net0 virtio,bridge=vmbr0 -cores 1 -sockets 1 -cpu cputype=kvm64 -description "debbie image" -kvm 1 -numa 1
-	qm importdisk "${VMID}" debian-11-openstack-amd64.qcow2 local-zfs
+	qm create "${VMID}" -name "${VMNAME}" -memory 1024 -net0 virtio,bridge=vmbr0 -cores 1 -sockets 1 -cpu cputype=kvm64 -description "debian 11 template" -kvm 1 -numa 1
+	qm importdisk "${VMID}" debian-11-amd64.qcow2 local-zfs
 	qm set "${VMID}" -scsihw virtio-scsi-pci -virtio0 local-zfs:vm-"${VMID}"-disk-0
 	qm set "${VMID}" -serial0 socket
 	qm set "${VMID}" -boot c -bootdisk virtio0
-#	qm set "${VMID}" -agent 1
+	qm set "${VMID}" -agent 1
 	qm set "${VMID}" -hotplug disk,network,usb,memory,cpu
 	qm set "${VMID}" -vcpus 1
 	qm set "${VMID}" -vga qxl
