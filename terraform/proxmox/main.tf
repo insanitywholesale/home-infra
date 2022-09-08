30348df5917b40261c3b263a8f6a8f07002386ec
insanitywholesale <popomastor@gmail.com>
2022-09-08 21:46:12 +0300

    remove pve0 from terraform

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -8,63 +8,12 @@ terraform {
 }
 
 provider "proxmox" {
-  alias           = "pve0"
-  pm_tls_insecure = true
-  pm_api_url      = "https://10.99.50.70:8006/api2/json"
-  pm_password     = "failfail"
-  pm_user         = "root@pam"
-  pm_timeout      = 900
-}
-
-provider "proxmox" {
   alias           = "pve1"
   pm_tls_insecure = true
   pm_api_url      = "https://10.99.50.71:8006/api2/json"
   pm_password     = "failfail"
   pm_user         = "root@pam"
   pm_timeout      = 900
-}
-
-resource "proxmox_vm_qemu" "proxmox_vm_k3s" {
-  provider    = proxmox.pve0
-  count       = 4
-  name        = "deb-k3s-${count.index + 1}"
-  desc        = "non-HA k3s cluster host ${count.index + 1}"
-  target_node = "pve0"
-
-  clone    = "debian-11-template"
-  os_type  = "cloud-init"
-  cores    = 2
-  sockets  = 1
-  cpu      = "host"
-  memory   = 3072
-  scsihw   = "virtio-scsi-pci"
-  bootdisk = "virtio0"
-  agent    = 1
-  onboot   = true
-
-  disk {
-    size    = "30G"
-    type    = "virtio"
-    storage = "local-zfs"
-  }
-
-  network {
-    model  = "virtio"
-    bridge = "vmbr1"
-  }
-
-  ipconfig0 = "ip=10.0.50.4${count.index + 1}/24,gw=10.0.50.254"
-
-  sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5jzKi37jm3517bqThbw+7LR/GXm3qC6Az5F+ZUa36vYM7Ygk2K5bWcFIL2YUCrkL5jfSsvoowONjCAxyuoyxtW4MJxnQLyq4u4yDsRC7YvBPAKZUYaHwnbkCfDs5a75dEFOoDxCA0DY2GrhqzBndaTcCfl0fZ4vN+9LcKOb1dSKiHeHvsh35YNtwntbL21meo+hiycUEgGwNe9/4kxKpdGTr7HvbeX2Fjm/UZBZIJKVcGop/3gCHXYnKH+OY5zc8cmt9Jg4CIwEqrSKeOX0bE8LSPRpVRXH4v8OcMaMei/HQejlH8NBwybEdJ4mhl8vHaFEjDbIWoOujmiRQF2263 angle@puddle"
-
-  lifecycle {
-    ignore_changes = [
-      cipassword,
-      network,
-      desc,
-    ]
-  }
 }
 
 resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters" {
@@ -240,7 +189,6 @@ resource "local_file" "hosts_cfg_k3s" {
   content = templatefile(
     "${path.module}/inventory/k3s-inventory.ini.tftpl",
     {
-      k3s_hosts   = proxmox_vm_qemu.proxmox_vm_k3s.*,
       k3s_masters = proxmox_vm_qemu.proxmox_vm_k3s_ha_masters.*,
       k3s_workers = proxmox_vm_qemu.proxmox_vm_k3s_ha_workers.*,
     }
