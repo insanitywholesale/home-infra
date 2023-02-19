469d5e0efbf7ed37213ea844d53794f25377924f
insanitywholesale <popomastor@gmail.com>
2023-02-19 14:35:07 +0200

    clean up main.tf

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -16,6 +16,15 @@ provider "proxmox" {
   pm_timeout      = 900
 }
 
+provider "proxmox" {
+  alias           = "pve2"
+  pm_tls_insecure = true
+  pm_api_url      = "https://10.0.50.72:8006/api2/json"
+  pm_password     = "failfail"
+  pm_user         = "root@pam"
+  pm_timeout      = 900
+}
+
 resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters" {
   provider    = proxmox.pve1
   count       = 1
@@ -88,132 +97,6 @@ resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_workers" {
   }
 
   ipconfig0 = "ip=10.0.50.${count.index + 61}/24,gw=10.0.50.254"
-
-  sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5jzKi37jm3517bqThbw+7LR/GXm3qC6Az5F+ZUa36vYM7Ygk2K5bWcFIL2YUCrkL5jfSsvoowONjCAxyuoyxtW4MJxnQLyq4u4yDsRC7YvBPAKZUYaHwnbkCfDs5a75dEFOoDxCA0DY2GrhqzBndaTcCfl0fZ4vN+9LcKOb1dSKiHeHvsh35YNtwntbL21meo+hiycUEgGwNe9/4kxKpdGTr7HvbeX2Fjm/UZBZIJKVcGop/3gCHXYnKH+OY5zc8cmt9Jg4CIwEqrSKeOX0bE8LSPRpVRXH4v8OcMaMei/HQejlH8NBwybEdJ4mhl8vHaFEjDbIWoOujmiRQF2263 angle@puddle"
-
-  lifecycle {
-    ignore_changes = [
-      cipassword,
-      network,
-      desc,
-    ]
-  }
-}
-
-resource "proxmox_vm_qemu" "proxmox_vm_fog" {
-  provider    = proxmox.pve1
-  count       = 0
-  name        = "deb-fog-1"
-  desc        = "fog PXE server"
-  target_node = "pve1"
-
-  clone    = "debian-11-template"
-  os_type  = "cloud-init"
-  cores    = 1
-  sockets  = 1
-  cpu      = "host"
-  memory   = 2048
-  scsihw   = "virtio-scsi-pci"
-  bootdisk = "virtio0"
-  agent    = 1
-  onboot   = true
-
-  disk {
-    size    = "40G"
-    type    = "virtio"
-    storage = "local-zfs"
-  }
-
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-
-  ipconfig0 = "ip=10.0.50.26/24,gw=10.0.50.254"
-
-  sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5jzKi37jm3517bqThbw+7LR/GXm3qC6Az5F+ZUa36vYM7Ygk2K5bWcFIL2YUCrkL5jfSsvoowONjCAxyuoyxtW4MJxnQLyq4u4yDsRC7YvBPAKZUYaHwnbkCfDs5a75dEFOoDxCA0DY2GrhqzBndaTcCfl0fZ4vN+9LcKOb1dSKiHeHvsh35YNtwntbL21meo+hiycUEgGwNe9/4kxKpdGTr7HvbeX2Fjm/UZBZIJKVcGop/3gCHXYnKH+OY5zc8cmt9Jg4CIwEqrSKeOX0bE8LSPRpVRXH4v8OcMaMei/HQejlH8NBwybEdJ4mhl8vHaFEjDbIWoOujmiRQF2263 angle@puddle"
-
-  lifecycle {
-    ignore_changes = [
-      cipassword,
-      network,
-      desc,
-    ]
-  }
-}
-
-resource "proxmox_vm_qemu" "proxmox_vm_mc" {
-  provider    = proxmox.pve1
-  count       = 0
-  name        = "deb-mc-1"
-  desc        = "emma stoners minecraft"
-  target_node = "pve1"
-
-  clone    = "debian-11-template"
-  os_type  = "cloud-init"
-  cores    = 3
-  sockets  = 1
-  cpu      = "host"
-  memory   = 7168
-  scsihw   = "virtio-scsi-pci"
-  bootdisk = "virtio0"
-  agent    = 1
-  onboot   = true
-
-  disk {
-    size    = "50G"
-    type    = "virtio"
-    storage = "local-zfs"
-  }
-
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-
-  ipconfig0 = "ip=10.0.50.85/24,gw=10.0.50.254"
-
-  sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5jzKi37jm3517bqThbw+7LR/GXm3qC6Az5F+ZUa36vYM7Ygk2K5bWcFIL2YUCrkL5jfSsvoowONjCAxyuoyxtW4MJxnQLyq4u4yDsRC7YvBPAKZUYaHwnbkCfDs5a75dEFOoDxCA0DY2GrhqzBndaTcCfl0fZ4vN+9LcKOb1dSKiHeHvsh35YNtwntbL21meo+hiycUEgGwNe9/4kxKpdGTr7HvbeX2Fjm/UZBZIJKVcGop/3gCHXYnKH+OY5zc8cmt9Jg4CIwEqrSKeOX0bE8LSPRpVRXH4v8OcMaMei/HQejlH8NBwybEdJ4mhl8vHaFEjDbIWoOujmiRQF2263 angle@puddle"
-
-  lifecycle {
-    ignore_changes = [
-      cipassword,
-      network,
-      desc,
-    ]
-  }
-}
-
-resource "proxmox_vm_qemu" "proxmox_vm_arch" {
-  provider    = proxmox.pve1
-  count       = 0
-  name        = "arch-${count.index + 1}"
-  target_node = "pve1"
-
-  /* change to debian-templ for 11 */
-  clone    = "arch-tmpl"
-  os_type  = "cloud-init"
-  cores    = 2
-  sockets  = 1
-  cpu      = "host"
-  memory   = 1024
-  scsihw   = "virtio-scsi-pci"
-  bootdisk = "virtio0"
-  agent    = 1
-  onboot   = true
-
-  disk {
-    size    = "20G"
-    type    = "virtio"
-    storage = "local-zfs"
-  }
-
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-
-  ipconfig0 = "ip=10.0.50.16${count.index}/24,gw=10.0.50.254"
 
   sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5jzKi37jm3517bqThbw+7LR/GXm3qC6Az5F+ZUa36vYM7Ygk2K5bWcFIL2YUCrkL5jfSsvoowONjCAxyuoyxtW4MJxnQLyq4u4yDsRC7YvBPAKZUYaHwnbkCfDs5a75dEFOoDxCA0DY2GrhqzBndaTcCfl0fZ4vN+9LcKOb1dSKiHeHvsh35YNtwntbL21meo+hiycUEgGwNe9/4kxKpdGTr7HvbeX2Fjm/UZBZIJKVcGop/3gCHXYnKH+OY5zc8cmt9Jg4CIwEqrSKeOX0bE8LSPRpVRXH4v8OcMaMei/HQejlH8NBwybEdJ4mhl8vHaFEjDbIWoOujmiRQF2263 angle@puddle"
 
