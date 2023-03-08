7bbe0c25e0be2b5b2e60caba948ce3307b5c61af
insanitywholesale <popomastor@gmail.com>
2023-03-08 21:46:19 +0200

    pave a new way

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -8,6 +8,15 @@ terraform {
 }
 
 provider "proxmox" {
+  alias           = "pve0"
+  pm_tls_insecure = true
+  pm_api_url      = "https://10.0.50.70:8006/api2/json"
+  pm_password     = "failfail"
+  pm_user         = "root@pam"
+  pm_timeout      = 900
+}
+
+provider "proxmox" {
   alias           = "pve1"
   pm_tls_insecure = true
   pm_api_url      = "https://10.0.50.71:8006/api2/json"
@@ -25,12 +34,12 @@ provider "proxmox" {
   pm_timeout      = 900
 }
 
-resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters" {
-  provider    = proxmox.pve1
+resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters_0" {
+  provider    = proxmox.pve0
   count       = 1
-  name        = "deb-k3s-m-${count.index + 1}"
-  desc        = "HA k3s cluster master host ${count.index + 1}"
-  target_node = "pve1"
+  name        = "deb-k3s-m-${(count.index * 2) + 1}"
+  desc        = "HA k3s cluster master host"
+  target_node = "pve0"
 
   clone    = "debian-11-template"
   os_type  = "cloud-init"
@@ -46,7 +55,7 @@ resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters" {
   disk {
     size    = "30G"
     type    = "virtio"
-    storage = "local-zfs"
+    storage = "local-lvm"
   }
 
   network {
@@ -54,7 +63,7 @@ resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters" {
     bridge = "vmbr0"
   }
 
-  ipconfig0 = "ip=10.0.50.${count.index + 51}/24,gw=10.0.50.254"
+  ipconfig0 = "ip=10.0.50.${(count.index * 2) + 1 + 51}/24,gw=10.0.50.254"
 
   sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5jzKi37jm3517bqThbw+7LR/GXm3qC6Az5F+ZUa36vYM7Ygk2K5bWcFIL2YUCrkL5jfSsvoowONjCAxyuoyxtW4MJxnQLyq4u4yDsRC7YvBPAKZUYaHwnbkCfDs5a75dEFOoDxCA0DY2GrhqzBndaTcCfl0fZ4vN+9LcKOb1dSKiHeHvsh35YNtwntbL21meo+hiycUEgGwNe9/4kxKpdGTr7HvbeX2Fjm/UZBZIJKVcGop/3gCHXYnKH+OY5zc8cmt9Jg4CIwEqrSKeOX0bE8LSPRpVRXH4v8OcMaMei/HQejlH8NBwybEdJ4mhl8vHaFEjDbIWoOujmiRQF2263 angle@puddle"
 
@@ -67,11 +76,11 @@ resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters" {
   }
 }
 
-resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_workers" {
+resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters_1" {
   provider    = proxmox.pve1
-  count       = 3
-  name        = "deb-k3s-w-${count.index + 1}"
-  desc        = "HA k3s cluster worker host ${count.index + 1}"
+  count       = 1
+  name        = "deb-k3s-m-${(count.index * 2) + 2}"
+  desc        = "HA k3s cluster master host"
   target_node = "pve1"
 
   clone    = "debian-11-template"
@@ -88,7 +97,7 @@ resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_workers" {
   disk {
     size    = "30G"
     type    = "virtio"
-    storage = "local-zfs"
+    storage = "local-lvm"
   }
 
   network {
@@ -96,7 +105,174 @@ resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_workers" {
     bridge = "vmbr0"
   }
 
-  ipconfig0 = "ip=10.0.50.${count.index + 61}/24,gw=10.0.50.254"
+  ipconfig0 = "ip=10.0.50.${(count.index * 2) + 2 + 51}/24,gw=10.0.50.254"
+
+  sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5jzKi37jm3517bqThbw+7LR/GXm3qC6Az5F+ZUa36vYM7Ygk2K5bWcFIL2YUCrkL5jfSsvoowONjCAxyuoyxtW4MJxnQLyq4u4yDsRC7YvBPAKZUYaHwnbkCfDs5a75dEFOoDxCA0DY2GrhqzBndaTcCfl0fZ4vN+9LcKOb1dSKiHeHvsh35YNtwntbL21meo+hiycUEgGwNe9/4kxKpdGTr7HvbeX2Fjm/UZBZIJKVcGop/3gCHXYnKH+OY5zc8cmt9Jg4CIwEqrSKeOX0bE8LSPRpVRXH4v8OcMaMei/HQejlH8NBwybEdJ4mhl8vHaFEjDbIWoOujmiRQF2263 angle@puddle"
+
+  lifecycle {
+    ignore_changes = [
+      cipassword,
+      network,
+      desc,
+    ]
+  }
+}
+resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters_2" {
+  provider    = proxmox.pve2
+  count       = 1
+  name        = "deb-k3s-m-${(count.index * 2) + 3}"
+  desc        = "HA k3s cluster master host"
+  target_node = "pve2"
+
+  clone    = "debian-11-template"
+  os_type  = "cloud-init"
+  cores    = 2
+  sockets  = 1
+  cpu      = "host"
+  memory   = 4096
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "virtio0"
+  agent    = 1
+  onboot   = true
+
+  disk {
+    size    = "30G"
+    type    = "virtio"
+    storage = "local-lvm"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  ipconfig0 = "ip=10.0.50.${(count.index * 2) + 3 + 51}/24,gw=10.0.50.254"
+
+  sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5jzKi37jm3517bqThbw+7LR/GXm3qC6Az5F+ZUa36vYM7Ygk2K5bWcFIL2YUCrkL5jfSsvoowONjCAxyuoyxtW4MJxnQLyq4u4yDsRC7YvBPAKZUYaHwnbkCfDs5a75dEFOoDxCA0DY2GrhqzBndaTcCfl0fZ4vN+9LcKOb1dSKiHeHvsh35YNtwntbL21meo+hiycUEgGwNe9/4kxKpdGTr7HvbeX2Fjm/UZBZIJKVcGop/3gCHXYnKH+OY5zc8cmt9Jg4CIwEqrSKeOX0bE8LSPRpVRXH4v8OcMaMei/HQejlH8NBwybEdJ4mhl8vHaFEjDbIWoOujmiRQF2263 angle@puddle"
+
+  lifecycle {
+    ignore_changes = [
+      cipassword,
+      network,
+      desc,
+    ]
+  }
+}
+
+resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_workers_0" {
+  provider    = proxmox.pve0
+  count       = 1
+  name        = "deb-k3s-w-${(count.index * 2) + 1}"
+  desc        = "HA k3s cluster worker host"
+  target_node = "pve0"
+
+  clone    = "debian-11-template"
+  os_type  = "cloud-init"
+  cores    = 2
+  sockets  = 1
+  cpu      = "host"
+  memory   = 4096
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "virtio0"
+  agent    = 1
+  onboot   = true
+
+  disk {
+    size    = "30G"
+    type    = "virtio"
+    storage = "local-lvm"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  ipconfig0 = "ip=10.0.50.${(count.index * 2) + 1 + 61}/24,gw=10.0.50.254"
+
+  sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5jzKi37jm3517bqThbw+7LR/GXm3qC6Az5F+ZUa36vYM7Ygk2K5bWcFIL2YUCrkL5jfSsvoowONjCAxyuoyxtW4MJxnQLyq4u4yDsRC7YvBPAKZUYaHwnbkCfDs5a75dEFOoDxCA0DY2GrhqzBndaTcCfl0fZ4vN+9LcKOb1dSKiHeHvsh35YNtwntbL21meo+hiycUEgGwNe9/4kxKpdGTr7HvbeX2Fjm/UZBZIJKVcGop/3gCHXYnKH+OY5zc8cmt9Jg4CIwEqrSKeOX0bE8LSPRpVRXH4v8OcMaMei/HQejlH8NBwybEdJ4mhl8vHaFEjDbIWoOujmiRQF2263 angle@puddle"
+
+  lifecycle {
+    ignore_changes = [
+      cipassword,
+      network,
+      desc,
+    ]
+  }
+}
+
+resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_workers_1" {
+  provider    = proxmox.pve1
+  count       = 1
+  name        = "deb-k3s-w-${(count.index * 2) + 2}"
+  desc        = "HA k3s cluster worker host"
+  target_node = "pve1"
+
+  clone    = "debian-11-template"
+  os_type  = "cloud-init"
+  cores    = 2
+  sockets  = 1
+  cpu      = "host"
+  memory   = 4096
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "virtio0"
+  agent    = 1
+  onboot   = true
+
+  disk {
+    size    = "30G"
+    type    = "virtio"
+    storage = "local-lvm"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  ipconfig0 = "ip=10.0.50.${(count.index * 2) + 2 + 61}/24,gw=10.0.50.254"
+
+  sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5jzKi37jm3517bqThbw+7LR/GXm3qC6Az5F+ZUa36vYM7Ygk2K5bWcFIL2YUCrkL5jfSsvoowONjCAxyuoyxtW4MJxnQLyq4u4yDsRC7YvBPAKZUYaHwnbkCfDs5a75dEFOoDxCA0DY2GrhqzBndaTcCfl0fZ4vN+9LcKOb1dSKiHeHvsh35YNtwntbL21meo+hiycUEgGwNe9/4kxKpdGTr7HvbeX2Fjm/UZBZIJKVcGop/3gCHXYnKH+OY5zc8cmt9Jg4CIwEqrSKeOX0bE8LSPRpVRXH4v8OcMaMei/HQejlH8NBwybEdJ4mhl8vHaFEjDbIWoOujmiRQF2263 angle@puddle"
+
+  lifecycle {
+    ignore_changes = [
+      cipassword,
+      network,
+      desc,
+    ]
+  }
+}
+
+resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_workers_2" {
+  provider    = proxmox.pve2
+  count       = 1
+  name        = "deb-k3s-w-${(count.index * 2) + 3}"
+  desc        = "HA k3s cluster worker host"
+  target_node = "pve2"
+
+  clone    = "debian-11-template"
+  os_type  = "cloud-init"
+  cores    = 2
+  sockets  = 1
+  cpu      = "host"
+  memory   = 4096
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "virtio0"
+  agent    = 1
+  onboot   = true
+
+  disk {
+    size    = "30G"
+    type    = "virtio"
+    storage = "local-lvm"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  ipconfig0 = "ip=10.0.50.${(count.index * 2 ) + 3 + 61}/24,gw=10.0.50.254"
 
   sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5jzKi37jm3517bqThbw+7LR/GXm3qC6Az5F+ZUa36vYM7Ygk2K5bWcFIL2YUCrkL5jfSsvoowONjCAxyuoyxtW4MJxnQLyq4u4yDsRC7YvBPAKZUYaHwnbkCfDs5a75dEFOoDxCA0DY2GrhqzBndaTcCfl0fZ4vN+9LcKOb1dSKiHeHvsh35YNtwntbL21meo+hiycUEgGwNe9/4kxKpdGTr7HvbeX2Fjm/UZBZIJKVcGop/3gCHXYnKH+OY5zc8cmt9Jg4CIwEqrSKeOX0bE8LSPRpVRXH4v8OcMaMei/HQejlH8NBwybEdJ4mhl8vHaFEjDbIWoOujmiRQF2263 angle@puddle"
 
@@ -114,8 +290,8 @@ resource "local_file" "hosts_cfg_k3s" {
   content = templatefile(
     "${path.module}/inventory/k3s-inventory.ini.tftpl",
     {
-      k3s_masters = proxmox_vm_qemu.proxmox_vm_k3s_ha_masters.*,
-      k3s_workers = proxmox_vm_qemu.proxmox_vm_k3s_ha_workers.*,
+      k3s_masters = concat(proxmox_vm_qemu.proxmox_vm_k3s_ha_masters_0.*,proxmox_vm_qemu.proxmox_vm_k3s_ha_masters_1.*,proxmox_vm_qemu.proxmox_vm_k3s_ha_masters_2.*),
+      k3s_workers = concat(proxmox_vm_qemu.proxmox_vm_k3s_ha_workers_0.*,proxmox_vm_qemu.proxmox_vm_k3s_ha_workers_1.*,proxmox_vm_qemu.proxmox_vm_k3s_ha_workers_2.*),
     }
   )
   filename        = "inventory/k3s-inventory.ini"
--- terraform/proxmox/playbooks/files/setup-debian-11-cloudinit.sh
+++ terraform/proxmox/playbooks/files/setup-debian-11-cloudinit.sh
@@ -22,8 +22,8 @@ then
 	exit 0
 else
 	qm create "${VMID}" -name "${VMNAME}" -memory 1024 -net0 virtio,bridge=vmbr0 -cores 1 -sockets 1 -cpu cputype=kvm64 -description "debian 11 template" -kvm 1 -numa 1
-	qm importdisk "${VMID}" debian-11-amd64.qcow2 local-zfs
-	qm set "${VMID}" -scsihw virtio-scsi-pci -virtio0 local-zfs:vm-"${VMID}"-disk-0
+	qm importdisk "${VMID}" debian-11-amd64.qcow2 local-lvm
+	qm set "${VMID}" -scsihw virtio-scsi-pci -virtio0 local-lvm:vm-"${VMID}"-disk-0
 	qm set "${VMID}" -serial0 socket
 	qm set "${VMID}" -boot c -bootdisk virtio0
 	qm set "${VMID}" -agent 1
@@ -31,6 +31,6 @@ else
 	qm set "${VMID}" -vcpus 1
 	qm set "${VMID}" -vga qxl
 	qm set "${VMID}" -name "${VMNAME}"
-	qm set "${VMID}" -ide2 local-zfs:cloudinit
+	qm set "${VMID}" -ide2 local-lvm:cloudinit
 	qm template "${VMID}"
 fi
