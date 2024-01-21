aad784bc649d602636c7f246b9ae2b3521240eef
insanitywholesale <popomastor@gmail.com>
2024-01-21 15:53:57 +0200

    terraform fmt

--- terraform/proxmox-cluster/main.tf
+++ terraform/proxmox-cluster/main.tf
@@ -272,7 +272,7 @@ resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_workers_2" {
     bridge = "vmbr0"
   }
 
-  ipconfig0 = "ip=10.0.50.${(count.index * 2 ) + 3 + 61}/24,gw=10.0.50.254"
+  ipconfig0 = "ip=10.0.50.${(count.index * 2) + 3 + 61}/24,gw=10.0.50.254"
 
   sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5jzKi37jm3517bqThbw+7LR/GXm3qC6Az5F+ZUa36vYM7Ygk2K5bWcFIL2YUCrkL5jfSsvoowONjCAxyuoyxtW4MJxnQLyq4u4yDsRC7YvBPAKZUYaHwnbkCfDs5a75dEFOoDxCA0DY2GrhqzBndaTcCfl0fZ4vN+9LcKOb1dSKiHeHvsh35YNtwntbL21meo+hiycUEgGwNe9/4kxKpdGTr7HvbeX2Fjm/UZBZIJKVcGop/3gCHXYnKH+OY5zc8cmt9Jg4CIwEqrSKeOX0bE8LSPRpVRXH4v8OcMaMei/HQejlH8NBwybEdJ4mhl8vHaFEjDbIWoOujmiRQF2263 angle@puddle"
 
@@ -290,8 +290,8 @@ resource "local_file" "hosts_cfg_k3s" {
   content = templatefile(
     "${path.module}/inventory/k3s-inventory.ini.tftpl",
     {
-      k3s_masters = concat(proxmox_vm_qemu.proxmox_vm_k3s_ha_masters_0.*,proxmox_vm_qemu.proxmox_vm_k3s_ha_masters_1.*,proxmox_vm_qemu.proxmox_vm_k3s_ha_masters_2.*),
-      k3s_workers = concat(proxmox_vm_qemu.proxmox_vm_k3s_ha_workers_0.*,proxmox_vm_qemu.proxmox_vm_k3s_ha_workers_1.*,proxmox_vm_qemu.proxmox_vm_k3s_ha_workers_2.*),
+      k3s_masters = concat(proxmox_vm_qemu.proxmox_vm_k3s_ha_masters_0.*, proxmox_vm_qemu.proxmox_vm_k3s_ha_masters_1.*, proxmox_vm_qemu.proxmox_vm_k3s_ha_masters_2.*),
+      k3s_workers = concat(proxmox_vm_qemu.proxmox_vm_k3s_ha_workers_0.*, proxmox_vm_qemu.proxmox_vm_k3s_ha_workers_1.*, proxmox_vm_qemu.proxmox_vm_k3s_ha_workers_2.*),
     }
   )
   filename        = "inventory/k3s-inventory.ini"
