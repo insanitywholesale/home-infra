d0e7c878e1290e6a684443c7e05a1e5ae043ca3c
insanitywholesale <popomastor@gmail.com>
2024-01-27 22:13:33 +0200

    restructure

--- terraform/proxmox-cluster/scripts/build_up.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-terraform plan -out tf-plans/create-k3s-vms.plan
-terraform apply -parallelism=3 tf-plans/create-k3s-vms.plan
-ansible-playbook -i inventory/k3s-inventory.ini playbooks/kates.yml
-ansible-playbook -i inventory/k3s-inventory.ini playbooks/k3s-site.yml
-ansible-playbook -i inventory/k3s-inventory.ini playbooks/k3s-ha-site.yml
