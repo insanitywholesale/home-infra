5b3a4a612f1702290587d82fb00c2e1489ff124b
insanitywholesale <popomastor@gmail.com>
2024-01-30 19:55:38 +0200

    add tf fog

---
+++ terraform/fog.tf
@@ -0,0 +1,44 @@
+resource "proxmox_vm_qemu" "proxmox_vm_fog" {
+  provider    = proxmox.pve1
+  vmid        = 1007
+  count       = 1
+  name        = format("fog%02svp", (count.index) + 1)
+  desc        = format("FOG PXE server %02s", (count.index) + 1)
+  target_node = "pve2"
+
+  clone    = "deb11-tmpl"
+  os_type  = "cloud-init"
+  cpu      = "SandyBridge"
+  cores    = 1
+  sockets  = 1
+  memory   = 1536
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "virtio0"
+  agent    = 1
+  onboot   = true
+
+  disk {
+    size    = "30G"
+    type    = "virtio"
+    storage = "local-lvm"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  ipconfig0 = "ip=10.0.50.${(count.index) + 7}/24,gw=10.0.50.254"
+
+  sshkeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgah15+jjufEiziZxhrmus/EVq9gPRqHMX5Ejl5dtWk angle"
+
+  tags = "debian;fog;pxe"
+
+  lifecycle {
+    ignore_changes = [
+      cipassword,
+      network,
+    ]
+  }
+}
+
