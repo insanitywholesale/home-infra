b533ddd49babf8fb099095633d24139ee1e6f5ed
insanitywholesale <popomastor@gmail.com>
2024-01-30 21:27:30 +0200

    add pihole vm

---
+++ terraform/pihole.tf
@@ -0,0 +1,44 @@
+resource "proxmox_vm_qemu" "proxmox_vm_pihole" {
+  provider    = proxmox.pve1
+  vmid        = 1002
+  count       = 1
+  name        = format("pihole%02svp", (count.index) + 1)
+  desc        = format("pihole %02s", (count.index) + 1)
+  target_node = "pve1"
+
+  clone    = "deb11-tmpl"
+  os_type  = "cloud-init"
+  cpu      = "SandyBridge"
+  cores    = 1
+  sockets  = 1
+  memory   = 1536
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "virtio0"
+  agent    = 1
+  onboot   = true
+
+  disk {
+    size    = "30G"
+    type    = "virtio"
+    storage = "local-lvm"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  ipconfig0 = "ip=10.0.50.${(count.index) + 2}/24,gw=10.0.50.254"
+
+  sshkeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgah15+jjufEiziZxhrmus/EVq9gPRqHMX5Ejl5dtWk angle"
+
+  tags = "debian;dns;pihole"
+
+  lifecycle {
+    ignore_changes = [
+      cipassword,
+      network,
+    ]
+  }
+}
+
