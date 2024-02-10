24d421fc64fd825b270ce7bda3b0ee3171d229f8
insanitywholesale <popomastor@gmail.com>
2024-02-10 10:54:02 +0200

    ansible lint autofix

--- ansible/generate_yaml_inventory.sh
+++ ansible/generate_yaml_inventory.sh
@@ -2,4 +2,5 @@
 
 cat inventory/*.ini > inventory/inventory.ini
 ansible-inventory -i inventory/inventory.ini -y --list > inventory/inventory.yaml
+ansible-lint --fix inventory/inventory.yaml
 rm inventory/inventory.ini
