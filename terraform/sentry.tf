4646d89a128fe8bdf3f425453251bab30c3e5a4c
insanitywholesale <popomastor@gmail.com>
2024-02-10 19:11:51 +0200

    bump sentry resources

--- terraform/sentry.tf
+++ terraform/sentry.tf
@@ -12,7 +12,7 @@ resource "proxmox_vm_qemu" "proxmox_vm_sentry" {
   cpu      = "SandyBridge"
   cores    = 3
   sockets  = 1
-  memory   = 6144
+  memory   = 9216
   scsihw   = "virtio-scsi-pci"
   bootdisk = "virtio0"
   agent    = 1
