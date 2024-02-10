3749ddf3b0873117f1a66e7323ad82404910453b
insanitywholesale <popomastor@gmail.com>
2024-02-10 12:00:09 +0200

    add ansible observium

--- ansible/generate_yaml_inventory.sh
+++ ansible/generate_yaml_inventory.sh
@@ -2,5 +2,5 @@
 
 cat inventory/*.ini > inventory/inventory.ini
 ansible-inventory -i inventory/inventory.ini -y --list > inventory/inventory.yaml
-ansible-lint --fix inventory/inventory.yaml
+ansible-lint --fix inventory/inventory.yaml &> /dev/null
 rm inventory/inventory.ini
