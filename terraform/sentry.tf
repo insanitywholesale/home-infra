ed0217c86d650258bba95fdb4248c908d8cc7394
insanitywholesale <popomastor@gmail.com>
2024-02-10 16:36:19 +0200

    add tf sentry

---
+++ terraform/sentry.tf
@@ -0,0 +1,45 @@
+resource "proxmox_vm_qemu" "proxmox_vm_sentry" {
+  provider    = proxmox.pve2
+  vmid        = 1038
+  count       = 1
+  name        = format("sentry%02s", (count.index) + 1)
+  desc        = format("Sentry server %02s", (count.index) + 1)
+  target_node = "pve2"
+
+  clone    = "deb11-tmpl"
+  os_type  = "cloud-init"
+  qemu_os  = "l26"
+  cpu      = "SandyBridge"
+  cores    = 2
+  sockets  = 1
+  memory   = 4096
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "virtio0"
+  agent    = 1
+  onboot   = true
+
+  disk {
+    size    = "50G"
+    type    = "virtio"
+    storage = "local-lvm"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  ipconfig0 = "ip=10.0.50.${(count.index) + 38}/24,gw=10.0.50.254"
+
+  sshkeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgah15+jjufEiziZxhrmus/EVq9gPRqHMX5Ejl5dtWk angle"
+
+  tags = "debian;monitoring;sentry"
+
+  lifecycle {
+    ignore_changes = [
+      cipassword,
+      network,
+    ]
+  }
+}
+
