07f3fb3acbbd9dea3482ba3dd7ac009bf7b2c74c
insanitywholesale <popomastor@gmail.com>
2024-02-11 11:08:00 +0200

    fix k3s cluster 2 master vmids

--- terraform/k3s_servers_c02.tf
+++ terraform/k3s_servers_c02.tf
@@ -1,7 +1,7 @@
 resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters_0_cluster_2" {
   provider    = proxmox.pve0
   count       = 1
-  vmid        = 1000 + (count.index * 3) + 51
+  vmid        = 1000 + (count.index * 3) + 54
   name        = format("k3s-m%02s-c%02s", (count.index * 3) + 1, 2)
   desc        = format("HA k3s server/master %02s for cluster %02s", (count.index * 3) + 1, 2)
   target_node = "pve0"
@@ -29,7 +29,7 @@ resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters_0_cluster_2" {
     bridge = "vmbr0"
   }
 
-  ipconfig0 = "ip=10.0.50.${(count.index * 3) + 51}/24,gw=10.0.50.254"
+  ipconfig0 = "ip=10.0.50.${(count.index * 3) + 54}/24,gw=10.0.50.254"
 
   sshkeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgah15+jjufEiziZxhrmus/EVq9gPRqHMX5Ejl5dtWk angle"
 
@@ -46,7 +46,7 @@ resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters_0_cluster_2" {
 resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters_1_cluster_2" {
   provider    = proxmox.pve1
   count       = 1
-  vmid        = 1000 + (count.index * 3) + 52
+  vmid        = 1000 + (count.index * 3) + 55
   name        = format("k3s-m%02s-c%02s", (count.index * 3) + 2, 2)
   desc        = format("HA k3s server/master %02s for cluster %02s", (count.index * 3) + 2, 2)
   target_node = "pve1"
@@ -91,7 +91,7 @@ resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters_1_cluster_2" {
 resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters_2_cluster_2" {
   provider    = proxmox.pve2
   count       = 1
-  vmid        = 1000 + (count.index * 3) + 53
+  vmid        = 1000 + (count.index * 3) + 56
   name        = format("k3s-m%02s-c%02s", (count.index * 3) + 3, 2)
   desc        = format("HA k3s server/master %02s for cluster %02s", (count.index * 3) + 3, 2)
   target_node = "pve2"
