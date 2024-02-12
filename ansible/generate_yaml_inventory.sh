fefdc07d8fa091fe5bdb7c8bcaaa7444b4699ce9
insanitywholesale <popomastor@gmail.com>
2024-02-12 07:53:00 +0200

    .yaml to .yml

--- ansible/generate_yaml_inventory.sh
+++ ansible/generate_yaml_inventory.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cat inventory/*.ini > inventory/inventory.ini
-ansible-inventory -i inventory/inventory.ini -y --list > inventory/inventory.yaml
-ansible-lint --fix inventory/inventory.yaml &> /dev/null
+ansible-inventory -i inventory/inventory.ini -y --list > inventory/inventory.yml
+ansible-lint --fix inventory/inventory.yml &> /dev/null
 rm inventory/inventory.ini
