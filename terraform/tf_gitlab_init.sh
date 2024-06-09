8d1a2cc1148e394aa8630b10156afb583397a9a1
insanitywholesale <popomastor@gmail.com>
2024-06-09 11:08:10 +0300

    add script to initialize terraform gitlab

---
+++ terraform/tf_gitlab_init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+export GITLAB_ACCESS_TOKEN="$(pass gitlab-all-access-pat)"
+export TF_STATE_NAME="homelab-cluster"
+terraform init \
+	-backend-config="address=https://gitlab.com/api/v4/projects/23069628/terraform/state/$TF_STATE_NAME" \
+	-backend-config="lock_address=https://gitlab.com/api/v4/projects/23069628/terraform/state/$TF_STATE_NAME/lock" \
+	-backend-config="unlock_address=https://gitlab.com/api/v4/projects/23069628/terraform/state/$TF_STATE_NAME/lock" \
+	-backend-config="username=insanitywholesale" \
+	-backend-config="password=$GITLAB_ACCESS_TOKEN" \
+	-backend-config="lock_method=POST" \
+	-backend-config="unlock_method=DELETE" \
+	-backend-config="retry_wait_min=5"
