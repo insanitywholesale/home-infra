b31a0988cd310bcb5ecad9c28236045d4bd8dcf8
insanitywholesale <popomastor@gmail.com>
2024-07-21 13:56:46 +0300

    try to simplify k3s cluster config

--- terraform/k3s_servers_c01.tf
+++ terraform/k3s_servers_c01.tf
@@ -1,9 +1,9 @@
-resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters_0" {
-  count       = 1
-  vmid        = 1000 + (count.index * 3) + 51
-  name        = format("k3s-m%02s-c%02s", (count.index * 3) + 1, 1)
-  desc        = format("HA k3s server/master %02s for cluster %02s", (count.index * 3) + 1, 1)
-  target_node = "pve01"
+resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters" {
+  count       = 3
+  vmid        = 1051 + count.index
+  name        = format("k3s-m%02s-c%02s", count.index + 1, 1)
+  desc        = format("HA k3s server/master %02s for cluster %02s", count.index + 1, 1)
+  target_node = format("pve%02s", (count.index % 3) + 1)
 
   clone    = "deb11-tmpl"
   os_type  = "cloud-init"
@@ -40,119 +40,7 @@ resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters_0" {
     bridge = "vmbr0"
   }
 
-  ipconfig0 = "ip=10.0.50.${(count.index * 3) + 51}/24,gw=10.0.50.254"
-
-  sshkeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgah15+jjufEiziZxhrmus/EVq9gPRqHMX5Ejl5dtWk angle"
-
-  tags = format("cluster%02s;debian;k3s;master;server", 1)
-
-  lifecycle {
-    ignore_changes = [
-      cipassword,
-      network,
-    ]
-  }
-}
-
-resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters_1" {
-  count       = 1
-  vmid        = 1000 + (count.index * 3) + 52
-  name        = format("k3s-m%02s-c%02s", (count.index * 3) + 2, 1)
-  desc        = format("HA k3s server/master %02s for cluster %02s", (count.index * 3) + 2, 1)
-  target_node = "pve02"
-
-  clone    = "deb11-tmpl"
-  os_type  = "cloud-init"
-  qemu_os  = "l26"
-  cpu      = "SandyBridge"
-  cores    = 2
-  sockets  = 1
-  memory   = 4096
-  scsihw   = "virtio-scsi-pci"
-  bootdisk = "virtio0"
-  agent    = 1
-  onboot   = true
-
-  disks {
-    ide {
-      ide3 {
-        cloudinit {
-          storage = "local-lvm"
-        }
-      }
-    }
-    virtio {
-      virtio0 {
-        disk {
-          size    = 40
-          storage = "local-lvm"
-        }
-      }
-    }
-  }
-
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-
-  ipconfig0 = "ip=10.0.50.${(count.index * 3) + 52}/24,gw=10.0.50.254"
-
-  sshkeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgah15+jjufEiziZxhrmus/EVq9gPRqHMX5Ejl5dtWk angle"
-
-  tags = format("cluster%02s;debian;k3s;master;server", 1)
-
-  lifecycle {
-    ignore_changes = [
-      cipassword,
-      network,
-    ]
-  }
-}
-
-resource "proxmox_vm_qemu" "proxmox_vm_k3s_ha_masters_2" {
-  count       = 1
-  vmid        = 1000 + (count.index * 3) + 53
-  name        = format("k3s-m%02s-c%02s", (count.index * 3) + 3, 1)
-  desc        = format("HA k3s server/master %02s for cluster %02s", (count.index * 3) + 3, 1)
-  target_node = "pve03"
-
-  clone    = "deb11-tmpl"
-  os_type  = "cloud-init"
-  qemu_os  = "l26"
-  cpu      = "SandyBridge"
-  cores    = 2
-  sockets  = 1
-  memory   = 4096
-  scsihw   = "virtio-scsi-pci"
-  bootdisk = "virtio0"
-  agent    = 1
-  onboot   = true
-
-  disks {
-    ide {
-      ide3 {
-        cloudinit {
-          storage = "local-lvm"
-        }
-      }
-    }
-    virtio {
-      virtio0 {
-        disk {
-          size    = 40
-          storage = "local-lvm"
-        }
-      }
-    }
-  }
-
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-
-  ipconfig0 = "ip=10.0.50.${(count.index * 3) + 53}/24,gw=10.0.50.254"
+  ipconfig0 = "ip=10.0.50.${51 + count.index}/24,gw=10.0.50.254"
 
   sshkeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgah15+jjufEiziZxhrmus/EVq9gPRqHMX5Ejl5dtWk angle"
 
