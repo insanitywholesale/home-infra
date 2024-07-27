a0e503b3eaaabd1a8a81a0f20e15e174650eaff1
insanitywholesale <popomastor@gmail.com>
2024-07-27 21:55:26 +0300

    change api endpoint

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "proxmox" {
   pm_tls_insecure = true
-  pm_api_url      = "https://10.0.50.71:8006/api2/json"
+  pm_api_url      = "https://10.0.50.70:8006/api2/json"
   pm_password     = "failfail"
   pm_user         = "root@pam"
   pm_timeout      = 900
