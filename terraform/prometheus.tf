c2dd2970ad9fde049335309acb12e8484b635cba
insanitywholesale <popomastor@gmail.com>
2024-07-28 10:34:18 +0300

    working on proxmox ansible

--- terraform/prometheus.tf
+++ terraform/prometheus.tf
@@ -5,7 +5,7 @@ resource "proxmox_vm_qemu" "proxmox_vm_prometheus" {
   desc        = format("prometheus %02s", (count.index) + 1)
   target_node = "pve02"
 
-  clone    = "deb11-tmpl"
+  clone    = "deb12-tmpl"
   os_type  = "cloud-init"
   qemu_os  = "l26"
   cpu      = "SandyBridge"
