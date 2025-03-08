a2df1dd94c660ebdd4b51404c3cffd5200df44ba
insanitywholesale <popomastor@gmail.com>
2025-03-08 12:14:01 +0200

    rename cluster01 to homecluster

--- fluxcd/README.md
+++ fluxcd/README.md
@@ -4,5 +4,5 @@
 
 Run the following spaghetti:
 ```bash
-kubectl create ns flux-system; cat ~/age.agekey | kubectl -n flux-system create secret generic sops-age --from-file=age.agekey=/dev/stdin; export GITLAB_TOKEN=$(pass gitlab-all-access-pat); flux bootstrap gitlab --owner=insanitywholesale --repository=infra --visibility=public --personal --branch=master --path=fluxcd/cluster01/ --token-auth=false --read-write-key=true
+kubectl create ns flux-system; cat ~/age.agekey | kubectl -n flux-system create secret generic sops-age --from-file=age.agekey=/dev/stdin; export GITLAB_TOKEN=$(pass gitlab-all-access-pat); flux bootstrap gitlab --owner=insanitywholesale --repository=infra --visibility=public --personal --branch=master --path=fluxcd/homecluster/ --token-auth=false --read-write-key=true
 ```
--- recreate-flux.sh
+++ recreate-flux.sh
@@ -2,15 +2,15 @@
 
 # Commands to re-create everything
 
-mkdir -p fluxcd/cluster01/core/metallb/base    # For flux helmrepository, upstream chart helm values and flux helmrelease with custom values
-mkdir -p fluxcd/cluster01/core/metallb/config  # For things other than values of the upstream chart
+mkdir -p fluxcd/homecluster/core/metallb/base    # For flux helmrepository, upstream chart helm values and flux helmrelease with custom values
+mkdir -p fluxcd/homecluster/core/metallb/config  # For things other than values of the upstream chart
 
-cp fluxcd/cluster01/templates/hrhr-kustomization.yaml fluxcd/cluster01/core/metallb/base/kustomization.yaml
+cp fluxcd/homecluster/templates/hrhr-kustomization.yaml fluxcd/homecluster/core/metallb/base/kustomization.yaml
 
 flux create source helm metallb \
 	--interval=10m \
 	--url=https://metallb.github.io/metallb \
-	--export > fluxcd/cluster01/core/metallb/base/helmrepository.yaml
+	--export > fluxcd/homecluster/core/metallb/base/helmrepository.yaml
 
 flux create helmrelease metallb \
 	--interval=10m \
@@ -20,39 +20,39 @@ flux create helmrelease metallb \
 	--source=HelmRepository/metallb \
 	--chart=metallb \
 	--chart-version="0.14.9" \
-	--values=fluxcd/cluster01/core/metallb/base/metallb-values.yml \
-	--export > fluxcd/cluster01/core/metallb/base/helmrelease.yaml
+	--values=fluxcd/homecluster/core/metallb/base/metallb-values.yml \
+	--export > fluxcd/homecluster/core/metallb/base/helmrelease.yaml
 
 flux create kustomization metallb-base \
 	--interval=10m \
 	--source GitRepository/flux-system \
-	--path=fluxcd/cluster01/core/metallb/base \
+	--path=fluxcd/homecluster/core/metallb/base \
 	--prune \
 	--wait \
 	--decryption-provider=sops \
 	--decryption-secret=sops-age \
-	--export > fluxcd/cluster01/core/metallb/kustomization-fluxCRD.yaml
+	--export > fluxcd/homecluster/core/metallb/kustomization-fluxCRD.yaml
 
 flux create kustomization metallb-config \
 	--interval=10m \
 	--source GitRepository/flux-system \
-	--path=fluxcd/cluster01/core/metallb/config \
+	--path=fluxcd/homecluster/core/metallb/config \
 	--depends-on metallb-base \
 	--prune \
 	--wait \
 	--decryption-provider=sops \
 	--decryption-secret=sops-age \
-	--export >> fluxcd/cluster01/core/metallb/kustomization-fluxCRD.yaml
+	--export >> fluxcd/homecluster/core/metallb/kustomization-fluxCRD.yaml
 
-mkdir -p fluxcd/cluster01/core/cert-manager/base    # For flux helmrepository, upstream chart helm values and flux helmrelease with custom values
-mkdir -p fluxcd/cluster01/core/cert-manager/config  # For things other than values of the upstream chart
+mkdir -p fluxcd/homecluster/core/cert-manager/base    # For flux helmrepository, upstream chart helm values and flux helmrelease with custom values
+mkdir -p fluxcd/homecluster/core/cert-manager/config  # For things other than values of the upstream chart
 
-cp fluxcd/cluster01/templates/hrhr-kustomization.yaml fluxcd/cluster01/core/cert-manager/base/kustomization.yaml
+cp fluxcd/homecluster/templates/hrhr-kustomization.yaml fluxcd/homecluster/core/cert-manager/base/kustomization.yaml
 
 flux create source helm cert-manager \
 	--interval=10m \
 	--url=https://charts.jetstack.io \
-	--export > fluxcd/cluster01/core/cert-manager/base/helmrepository.yaml
+	--export > fluxcd/homecluster/core/cert-manager/base/helmrepository.yaml
 
 flux create helmrelease cert-manager \
 	--interval=10m \
@@ -62,38 +62,38 @@ flux create helmrelease cert-manager \
 	--source=HelmRepository/cert-manager \
 	--chart=cert-manager \
 	--chart-version="v1.17.1" \
-	--values=fluxcd/cluster01/core/cert-manager/base/cert-manager-values.yml \
-	--export > fluxcd/cluster01/core/cert-manager/base/helmrelease.yaml
+	--values=fluxcd/homecluster/core/cert-manager/base/cert-manager-values.yml \
+	--export > fluxcd/homecluster/core/cert-manager/base/helmrelease.yaml
 
 flux create kustomization cert-manager-base \
 	--interval=10m \
 	--source GitRepository/flux-system \
-	--path=fluxcd/cluster01/core/cert-manager/base \
+	--path=fluxcd/homecluster/core/cert-manager/base \
 	--prune \
 	--wait \
 	--decryption-provider=sops \
 	--decryption-secret=sops-age \
-	--export > fluxcd/cluster01/core/cert-manager/kustomization-fluxCRD.yaml
+	--export > fluxcd/homecluster/core/cert-manager/kustomization-fluxCRD.yaml
 
 flux create kustomization cert-manager-config \
 	--interval=10m \
 	--source GitRepository/flux-system \
-	--path=fluxcd/cluster01/core/cert-manager/config \
+	--path=fluxcd/homecluster/core/cert-manager/config \
 	--depends-on cert-manager-base \
 	--prune \
 	--wait \
 	--decryption-provider=sops \
 	--decryption-secret=sops-age \
-	--export >> fluxcd/cluster01/core/cert-manager/kustomization-fluxCRD.yaml
+	--export >> fluxcd/homecluster/core/cert-manager/kustomization-fluxCRD.yaml
 
-mkdir -p fluxcd/cluster01/core/ingress-nginx/base    # For flux helmrepository, upstream chart helm values and flux helmrelease with custom values
+mkdir -p fluxcd/homecluster/core/ingress-nginx/base    # For flux helmrepository, upstream chart helm values and flux helmrelease with custom values
 
-cp fluxcd/cluster01/templates/hrhr-kustomization.yaml fluxcd/cluster01/core/ingress-nginx/base/kustomization.yaml
+cp fluxcd/homecluster/templates/hrhr-kustomization.yaml fluxcd/homecluster/core/ingress-nginx/base/kustomization.yaml
 
 flux create source helm ingress-nginx \
 	--interval=10m \
 	--url=https://kubernetes.github.io/ingress-nginx \
-	--export > fluxcd/cluster01/core/ingress-nginx/base/helmrepository.yaml
+	--export > fluxcd/homecluster/core/ingress-nginx/base/helmrepository.yaml
 
 flux create helmrelease ingress-nginx \
 	--interval=10m \
@@ -103,28 +103,28 @@ flux create helmrelease ingress-nginx \
 	--source=HelmRepository/ingress-nginx \
 	--chart=ingress-nginx \
 	--chart-version="4.12.0" \
-	--values=fluxcd/cluster01/core/ingress-nginx/base/ingress-nginx-values.yml \
-	--export > fluxcd/cluster01/core/ingress-nginx/base/helmrelease.yaml
+	--values=fluxcd/homecluster/core/ingress-nginx/base/ingress-nginx-values.yml \
+	--export > fluxcd/homecluster/core/ingress-nginx/base/helmrelease.yaml
 
 flux create kustomization ingress-nginx-base \
 	--interval=10m \
 	--source GitRepository/flux-system \
-	--path=fluxcd/cluster01/core/ingress-nginx/base \
+	--path=fluxcd/homecluster/core/ingress-nginx/base \
 	--depends-on metallb-config \
 	--prune \
 	--wait \
 	--decryption-provider=sops \
 	--decryption-secret=sops-age \
-	--export > fluxcd/cluster01/core/ingress-nginx/kustomization-fluxCRD.yaml
+	--export > fluxcd/homecluster/core/ingress-nginx/kustomization-fluxCRD.yaml
 
-mkdir -p fluxcd/cluster01/core/external-dns/base    # For flux helmrepository, upstream chart helm values and flux helmrelease with custom values
+mkdir -p fluxcd/homecluster/core/external-dns/base    # For flux helmrepository, upstream chart helm values and flux helmrelease with custom values
 
-cp fluxcd/cluster01/templates/hrhr-kustomization.yaml fluxcd/cluster01/core/external-dns/base/kustomization.yaml
+cp fluxcd/homecluster/templates/hrhr-kustomization.yaml fluxcd/homecluster/core/external-dns/base/kustomization.yaml
 
 flux create source helm external-dns \
 	--interval=10m \
 	--url=https://kubernetes-sigs.github.io/external-dns \
-	--export > fluxcd/cluster01/core/external-dns/base/helmrepository.yaml
+	--export > fluxcd/homecluster/core/external-dns/base/helmrepository.yaml
 
 flux create helmrelease external-dns \
 	--interval=10m \
@@ -134,28 +134,28 @@ flux create helmrelease external-dns \
 	--source=HelmRepository/external-dns \
 	--chart=external-dns \
 	--chart-version="1.14.5" \
-	--values=fluxcd/cluster01/core/external-dns/base/external-dns-values.yml \
-	--export > fluxcd/cluster01/core/external-dns/base/helmrelease.yaml
+	--values=fluxcd/homecluster/core/external-dns/base/external-dns-values.yml \
+	--export > fluxcd/homecluster/core/external-dns/base/helmrelease.yaml
 
 flux create kustomization external-dns-base \
 	--interval=10m \
 	--source GitRepository/flux-system \
-	--path=fluxcd/cluster01/core/external-dns/base \
+	--path=fluxcd/homecluster/core/external-dns/base \
 	--depends-on "metallb-base,ingress-nginx-base" \
 	--prune \
 	--wait \
 	--decryption-provider=sops \
 	--decryption-secret=sops-age \
-	--export > fluxcd/cluster01/core/external-dns/kustomization-fluxCRD.yaml
+	--export > fluxcd/homecluster/core/external-dns/kustomization-fluxCRD.yaml
 
-mkdir -p fluxcd/cluster01/apps/lister/base    # For flux helmrepository, upstream chart helm values and flux helmrelease with custom values
+mkdir -p fluxcd/homecluster/apps/lister/base    # For flux helmrepository, upstream chart helm values and flux helmrelease with custom values
 
-cp fluxcd/cluster01/templates/hrhr-kustomization.yaml fluxcd/cluster01/apps/lister/base/kustomization.yaml
+cp fluxcd/homecluster/templates/hrhr-kustomization.yaml fluxcd/homecluster/apps/lister/base/kustomization.yaml
 
 flux create source helm lister \
 	--interval=10m \
 	--url=https://gitlab.com/api/v4/projects/27255221/packages/helm/stable \
-	--export > fluxcd/cluster01/apps/lister/base/helmrepository.yaml
+	--export > fluxcd/homecluster/apps/lister/base/helmrepository.yaml
 
 flux create helmrelease lister \
 	--interval=10m \
@@ -165,28 +165,28 @@ flux create helmrelease lister \
 	--source=HelmRepository/lister \
 	--chart=lister \
 	--chart-version="0.2.2" \
-	--values=fluxcd/cluster01/apps/lister/base/lister-values.yml \
-	--export > fluxcd/cluster01/apps/lister/base/helmrelease.yaml
+	--values=fluxcd/homecluster/apps/lister/base/lister-values.yml \
+	--export > fluxcd/homecluster/apps/lister/base/helmrelease.yaml
 
 flux create kustomization lister-base \
 	--interval=10m \
 	--source GitRepository/flux-system \
-	--path=fluxcd/cluster01/apps/lister/base \
+	--path=fluxcd/homecluster/apps/lister/base \
 	--depends-on ingress-nginx-base \
 	--prune \
 	--wait \
 	--decryption-provider=sops \
 	--decryption-secret=sops-age \
-	--export > fluxcd/cluster01/apps/lister/kustomization-fluxCRD.yaml
+	--export > fluxcd/homecluster/apps/lister/kustomization-fluxCRD.yaml
 
-mkdir -p fluxcd/cluster01/apps/dashboard/base    # For flux helmrepository, upstream chart helm values and flux helmrelease with custom values
+mkdir -p fluxcd/homecluster/apps/dashboard/base    # For flux helmrepository, upstream chart helm values and flux helmrelease with custom values
 
-cp fluxcd/cluster01/templates/hrhr-kustomization.yaml fluxcd/cluster01/apps/dashboard/base/kustomization.yaml
+cp fluxcd/homecluster/templates/hrhr-kustomization.yaml fluxcd/homecluster/apps/dashboard/base/kustomization.yaml
 
 flux create source helm dashboard \
 	--interval=10m \
 	--url=https://kubernetes.github.io/dashboard \
-	--export > fluxcd/cluster01/apps/dashboard/base/helmrepository.yaml
+	--export > fluxcd/homecluster/apps/dashboard/base/helmrepository.yaml
 
 flux create helmrelease dashboard \
 	--interval=10m \
@@ -196,28 +196,28 @@ flux create helmrelease dashboard \
 	--source=HelmRepository/dashboard \
 	--chart=kubernetes-dashboard \
 	--chart-version="7.5.0" \
-	--values=fluxcd/cluster01/apps/dashboard/base/dashboard-values.yml \
-	--export > fluxcd/cluster01/apps/dashboard/base/helmrelease.yaml
+	--values=fluxcd/homecluster/apps/dashboard/base/dashboard-values.yml \
+	--export > fluxcd/homecluster/apps/dashboard/base/helmrelease.yaml
 
 flux create kustomization dashboard-base \
 	--interval=10m \
 	--source GitRepository/flux-system \
-	--path=fluxcd/cluster01/apps/dashboard/base \
+	--path=fluxcd/homecluster/apps/dashboard/base \
 	--depends-on ingress-nginx-base \
 	--prune \
 	--wait \
 	--decryption-provider=sops \
 	--decryption-secret=sops-age \
-	--export > fluxcd/cluster01/apps/dashboard/kustomization-fluxCRD.yaml
+	--export > fluxcd/homecluster/apps/dashboard/kustomization-fluxCRD.yaml
 
-mkdir -p fluxcd/cluster01/apps/podinfo/base    # For flux helmrepository, upstream chart helm values and flux helmrelease with custom values
+mkdir -p fluxcd/homecluster/apps/podinfo/base    # For flux helmrepository, upstream chart helm values and flux helmrelease with custom values
 
-cp fluxcd/cluster01/templates/hrhr-kustomization.yaml fluxcd/cluster01/apps/podinfo/base/kustomization.yaml
+cp fluxcd/homecluster/templates/hrhr-kustomization.yaml fluxcd/homecluster/apps/podinfo/base/kustomization.yaml
 
  flux create source helm podinfo \
 	--interval=10m \
 	--url=https://stefanprodan.github.io/podinfo \
-	--export > fluxcd/cluster01/apps/podinfo/base/helmrepository.yaml
+	--export > fluxcd/homecluster/apps/podinfo/base/helmrepository.yaml
 
 flux create helmrelease podinfo \
 	--interval=10m \
@@ -227,31 +227,31 @@ flux create helmrelease podinfo \
 	--source=HelmRepository/podinfo \
 	--chart=podinfo \
 	--chart-version="6.7.0" \
-	--values=fluxcd/cluster01/apps/podinfo/base/podinfo-values.yml \
-	--export > fluxcd/cluster01/apps/podinfo/base/helmrelease.yaml
+	--values=fluxcd/homecluster/apps/podinfo/base/podinfo-values.yml \
+	--export > fluxcd/homecluster/apps/podinfo/base/helmrelease.yaml
 
 flux create kustomization podinfo-base \
 	--interval=10m \
 	--source GitRepository/flux-system \
-	--path=fluxcd/cluster01/apps/podinfo/base \
+	--path=fluxcd/homecluster/apps/podinfo/base \
 	--depends-on ingress-nginx-base \
 	--prune \
 	--wait \
 	--decryption-provider=sops \
 	--decryption-secret=sops-age \
-	--export > fluxcd/cluster01/apps/podinfo/kustomization-fluxCRD.yaml
+	--export > fluxcd/homecluster/apps/podinfo/kustomization-fluxCRD.yaml
 
 # https://raw.githubusercontent.com/openebs/openebs/e90fe9b35ea45ca03077317259e8eb530693ad33/charts/values.yaml
 
-mkdir -p fluxcd/cluster01/core/openebs/base    # For flux helmrepository, upstream chart helm values and flux helmrelease with custom values
-mkdir -p fluxcd/cluster01/core/openebs/config  # For things other than values of the upstream chart
+mkdir -p fluxcd/homecluster/core/openebs/base    # For flux helmrepository, upstream chart helm values and flux helmrelease with custom values
+mkdir -p fluxcd/homecluster/core/openebs/config  # For things other than values of the upstream chart
 
-cp fluxcd/cluster01/templates/hrhr-kustomization.yaml fluxcd/cluster01/core/openebs/base/kustomization.yaml
+cp fluxcd/homecluster/templates/hrhr-kustomization.yaml fluxcd/homecluster/core/openebs/base/kustomization.yaml
 
 flux create source helm openebs \
 	--interval=10m \
 	--url=https://openebs.github.io/openebs \
-	--export > fluxcd/cluster01/core/openebs/base/helmrepository.yaml
+	--export > fluxcd/homecluster/core/openebs/base/helmrepository.yaml
 
 flux create helmrelease openebs \
 	--interval=10m \
@@ -261,38 +261,38 @@ flux create helmrelease openebs \
 	--source=HelmRepository/openebs \
 	--chart=openebs \
 	--chart-version="4.1.3" \
-	--values=fluxcd/cluster01/core/openebs/base/openebs-values.yml \
-	--export > fluxcd/cluster01/core/openebs/base/helmrelease.yaml
+	--values=fluxcd/homecluster/core/openebs/base/openebs-values.yml \
+	--export > fluxcd/homecluster/core/openebs/base/helmrelease.yaml
 
 flux create kustomization openebs-base \
 	--interval=10m \
 	--source GitRepository/flux-system \
-	--path=fluxcd/cluster01/core/openebs/base \
+	--path=fluxcd/homecluster/core/openebs/base \
 	--prune \
 	--wait \
 	--decryption-provider=sops \
 	--decryption-secret=sops-age \
-	--export > fluxcd/cluster01/core/openebs/kustomization-fluxCRD.yaml
+	--export > fluxcd/homecluster/core/openebs/kustomization-fluxCRD.yaml
 
 flux create kustomization openebs-config \
 	--interval=10m \
 	--source GitRepository/flux-system \
-	--path=fluxcd/cluster01/core/openebs/config \
+	--path=fluxcd/homecluster/core/openebs/config \
 	--depends-on openebs-base \
 	--prune \
 	--wait \
 	--decryption-provider=sops \
 	--decryption-secret=sops-age \
-	--export >> fluxcd/cluster01/core/openebs/kustomization-fluxCRD.yaml
+	--export >> fluxcd/homecluster/core/openebs/kustomization-fluxCRD.yaml
 
-mkdir -p fluxcd/cluster01/apps/cnpg/base    # For flux helmrepository, upstream chart helm values and flux helmrelease with custom values
+mkdir -p fluxcd/homecluster/apps/cnpg/base    # For flux helmrepository, upstream chart helm values and flux helmrelease with custom values
 
-cp fluxcd/cluster01/templates/hrhr-kustomization.yaml fluxcd/cluster01/apps/cnpg/base/kustomization.yaml
+cp fluxcd/homecluster/templates/hrhr-kustomization.yaml fluxcd/homecluster/apps/cnpg/base/kustomization.yaml
 
 flux create source helm cnpg \
 	--interval=10m \
 	--url=https://cloudnative-pg.github.io/charts \
-	--export > fluxcd/cluster01/apps/cnpg/base/helmrepository.yaml
+	--export > fluxcd/homecluster/apps/cnpg/base/helmrepository.yaml
 
 flux create helmrelease cnpg \
 	--interval=10m \
@@ -302,15 +302,15 @@ flux create helmrelease cnpg \
 	--source=HelmRepository/cnpg \
 	--chart=cloudnative-pg \
 	--chart-version="0.23.2" \
-	--values=fluxcd/cluster01/apps/cnpg/base/cnpg-values.yml \
-	--export > fluxcd/cluster01/apps/cnpg/base/helmrelease.yaml
+	--values=fluxcd/homecluster/apps/cnpg/base/cnpg-values.yml \
+	--export > fluxcd/homecluster/apps/cnpg/base/helmrelease.yaml
 
 flux create kustomization cnpg-base \
 	--interval=10m \
 	--source GitRepository/flux-system \
-	--path=fluxcd/cluster01/apps/cnpg/base \
+	--path=fluxcd/homecluster/apps/cnpg/base \
 	--prune \
 	--wait \
 	--decryption-provider=sops \
 	--decryption-secret=sops-age \
-	--export > fluxcd/cluster01/apps/cnpg/kustomization-fluxCRD.yaml
+	--export > fluxcd/homecluster/apps/cnpg/kustomization-fluxCRD.yaml
